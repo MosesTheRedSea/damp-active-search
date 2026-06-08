ea10b86baae6182927e840187cbdb5382d2e8f23
Moses <MosesOluwatobiAdewolu@gmail.com>
2026-06-09 08:45:14 +0900

    Ignore build and log directories

--- ros2_ws/build/acoustic_msgs/CMakeFiles/acoustic_msgs_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/acoustic_msgs_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/acoustic_msgs_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ros2_ws/build/active_search/CMakeFiles/active_search_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/active_search_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/active_search_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
